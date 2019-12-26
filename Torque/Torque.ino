8bfd713713a4da69cdde454f73310e27ceda5792
lcedermaz <32801806+lcedermaz@users.noreply.github.com>
2019-12-26 11:31:53 -0300

    creacion de toque
    unificación de los códigos "tacometro" y "celda de carga"

---
+++ Torque/Torque.ino
@@ -0,0 +1,2 @@
+// codigo que junta las rpm y la medicion de la celda de carga para obtener el torque del equipo
+// de esta manera eliminamos 1 display, un rs485, un arduino uno. Reduciendo conexiones y posibilidades de ruido
